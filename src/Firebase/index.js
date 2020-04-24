555b42eb5c203941ad4b4251788e064446782da7
komalGoel0507 <62433811+komalGoel0507@users.noreply.github.com>
2020-04-24 16:31:28 +0530

    Komal Commited

--- src/Firebase/index.js
+++
@@ -1,5 +0,0 @@
-// import FirebaseContext, { withFirebase } from "./context";
-import Firebase from "./Init";
-
-export default Firebase;
-// export { FirebaseContext, withFirebase };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import EditMenu from "./components/Screen5/EditMenu";
-import { LoginPage } from "./components/Screen4/LoginPage";
-import UserHome from "./components/Screen7User/UserHome";
-import UserOrder from "./components/Screen8User/UserOrder";
-import HomeSingleOrderReruest from "./components/Screen6/HomeSingleOrderReruest";
 
 import * as serviceWorker from "./serviceWorker";
 import App from "./App";
-import FirebaseLogin from "./Firebase/FirebaseLogin";
-import HomeOrder from "./components/Screen10/HomeOrder";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HomeOrder />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
